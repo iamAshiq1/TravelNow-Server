ac8e9e9481721c15aa21f029a4dfa5b779599ba8
Md Ashikur Rahman <86632525+iamAshiq1@users.noreply.github.com>
2022-11-19 15:58:58 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,15 @@ const express = require("express");
 const { MongoClient } = require("mongodb");
 const cors = require("cors");
 const ObjectId = require("mongodb").ObjectId;
-require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.obi7m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = "mongodb+srv://Ashik:90875@cluster0.obi7m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
